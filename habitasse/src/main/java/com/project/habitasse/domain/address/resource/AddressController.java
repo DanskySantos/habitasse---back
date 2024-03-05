127164f9093564d3628cbbb0458fcb2c4a991635
danSky <dansky123@hotmail.com>
2024-03-04 23:24:34 -0300

    creating save address endpoint

--- habitasse/src/main/java/com/project/habitasse/domain/address/resource/AddressController.java
+++ habitasse/src/main/java/com/project/habitasse/domain/address/resource/AddressController.java
@@ -1,9 +1,7 @@
 package com.project.habitasse.domain.address.resource;
 
+import com.project.habitasse.domain.address.entities.request.AddressRequest;
 import com.project.habitasse.domain.address.service.AddressService;
-import com.project.habitasse.domain.propertyDemand.entities.request.PropertyDemandRequest;
-import com.project.habitasse.domain.propertyDemand.service.PropertyDemandService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,15 +24,15 @@ public class AddressController {
         return ResponseEntity.ok(addressService.filterCities(stateName));
     }
 
-//    @GetMapping("/findByEmail/{email}")
-//    public ResponseEntity<List<PropertyDemand>> findByEmail(@PathVariable String email) {
-//        Optional<List<PropertyDemand>> propertyDemands = propertyDemandService.findByEmail(email);
-//        if (propertyDemands.isPresent()) {
-//            return new ResponseEntity<>(propertyDemands.get(), HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
+//    @PostMapping("/save")
+//    public ResponseEntity<?> save(@RequestBody AddressRequest addressRequest) throws Exception {
+//        if (userService.findByEmail(registerRequest.getEmail()).isPresent())
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Já existe um usuário cadastrado com esse e-mail");
+//
+//        if (registerRequest.getUserRoles() == null)
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Role não pode ser nula");
+//
+//        return ResponseEntity.ok(userService.register(registerRequest));
 //    }
-
 }
 

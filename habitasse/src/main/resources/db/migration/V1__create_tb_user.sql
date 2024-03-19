1b7fbfdc1c5feaf0758e2a6f2346b74ede0fc85a
danSky <dansky123@hotmail.com>
2024-03-19 16:20:43 -0300

    prod configuration

--- habitasse/src/main/resources/db/migration/V1__create_tb_user.sql
+++ habitasse/src/main/resources/db/migration/V1__create_tb_user.sql
@@ -29,6 +29,6 @@ create table tb_person
 alter table tb_person
     add constraint UK_86cal5mg9j0t8j9yn28n9iulc unique (uuid);
 alter table tb_user
-    add constraint UK_86cal5mg9j0t8j9yn28n9iulq unique (uuid);
+    add constraint UK_86cal5mg9j0t8j9yn28n9ijjj unique (uuid);
 alter table tb_user
     add constraint FK349pmtlc4ukgn1so04k2ls4jj foreign key (person_id) references tb_person;

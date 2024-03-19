b691c2f4ef0af9ff865af98c39c14b9ca3c363dc
danSky <dansky123@hotmail.com>
2024-03-19 15:42:52 -0300

    prod configuration

--- habitasse/src/main/java/com/project/habitasse/HabitasseApplication.java
+++ habitasse/src/main/java/com/project/habitasse/HabitasseApplication.java
@@ -2,12 +2,14 @@ package com.project.habitasse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
-public class HabitasseApplication {
+public class HabitasseApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HabitasseApplication.class, args);
 	}
-
 }
